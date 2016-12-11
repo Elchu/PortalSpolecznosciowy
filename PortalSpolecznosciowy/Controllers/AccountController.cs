e50ddacfa916baafaa805ded92181092e10c8e01
Mazik <Mazik@MAZIK-KOMPUTER>
2016-12-11 18:56:55 +0100

    Delete UserName whit form

--- PortalSpolecznosciowy/Controllers/AccountController.cs
+++ PortalSpolecznosciowy/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace PortalSpolecznosciowy.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, Sex = model.Sex};
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Sex = model.Sex};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
